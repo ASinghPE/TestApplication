9094c50522b5975ab8677f8ad79b57ebad61d6bb
Arshdeep Singh <arshdeep.singh@perkinelmer.com>
2022-10-03 10:51:06 -0400

    Add project files.

---
+++ JenkinsRunTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsRunTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsRunTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca62b1a1-861d-4adb-aff0-dd58b0dcf7d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JenkinsRunTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace JenkinsRunTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
